5e84e5162ff8357569c4cfaec6c0eed17fea46a3
Jyoti RS <jyotiranjans@cnxsi.com>
2020-03-04 08:34:46 +0530

    navigation version 5, first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {View, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home screen</Text>
+    </View>
+  );
+}
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Detail">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen
+          name="Detail"
+          component={DetailsScreen}
+          options={{title: 'jyoit'}}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
